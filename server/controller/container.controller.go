46cc5d6a6fca2fddd9a0a61727463f0db6277ef9
Vinícius do Carmo <vinimdocarmo@gmail.com>
2021-08-09 00:27:40 +0200

    chore: refactor api

---
+++ server/controller/container.controller.go
@@ -0,0 +1,198 @@
+package controller
+
+import (
+	"bufio"
+	"context"
+	"encoding/binary"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+type LogMessage struct {
+	Timestamp string `json:"timestamp"`
+	Message   string `json:"message"`
+}
+
+type DockerController struct {
+	DockerClient *client.Client
+}
+
+func (ctrl *DockerController) Remove(c *gin.Context) {
+	id := c.Params.ByName("id")
+	err := ctrl.DockerClient.ContainerRemove(c.Request.Context(), id, types.ContainerRemoveOptions{Force: true})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Writer.WriteHeader(http.StatusOK)
+}
+
+func (ctrl *DockerController) Start(c *gin.Context) {
+	id := c.Params.ByName("id")
+	err := ctrl.DockerClient.ContainerStart(c.Request.Context(), id, types.ContainerStartOptions{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Writer.WriteHeader(http.StatusOK)
+}
+
+func (ctrl *DockerController) Stop(c *gin.Context) {
+	id := c.Params.ByName("id")
+	err := ctrl.DockerClient.ContainerStop(c.Request.Context(), id, nil)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Writer.WriteHeader(http.StatusOK)
+}
+
+func (ctrl *DockerController) List(c *gin.Context) {
+	containers, err := ctrl.DockerClient.ContainerList(c.Request.Context(), types.ContainerListOptions{All: true})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, containers)
+}
+
+func (ctrl *DockerController) Logs(c *gin.Context) {
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			for _, origin := range r.Header["Origin"] {
+				if origin == "http://localhost:3000" {
+					return true
+				}
+			}
+			return false
+		},
+	}
+
+	w, r := c.Writer, c.Request
+	id := c.Params.ByName("id")
+
+	// Upgrade our raw HTTP connection to a websocket based one
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	defer conn.Close()
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	reader, err := cli.ContainerLogs(context.Background(), id, types.ContainerLogsOptions{Tail: "100", Follow: true, ShowStdout: true, ShowStderr: true, Timestamps: true})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	defer reader.Close()
+
+	container, err := cli.ContainerInspect(context.Background(), id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if !container.Config.Tty {
+		for {
+			header := make([]byte, 8)
+
+			n, err := reader.Read(header)
+
+			if err != nil || n != 8 {
+				if err == io.EOF {
+					return
+				}
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			timestamp := make([]byte, 31) // 30 bytes for timestamp and 1 for the following space
+
+			n, err = reader.Read(timestamp)
+
+			if err != nil || n != 31 {
+				if err == io.EOF {
+					return
+				}
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			outputSize := binary.BigEndian.Uint32(header[4:])
+			output := make([]byte, outputSize-uint32(n)) // makes sure to remove size of timestamp read
+			n, err = reader.Read(output)
+
+			if err != nil {
+				if err == io.EOF {
+					return
+				}
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			logMessage := LogMessage{
+				Message:   string(output[:n]),
+				Timestamp: string(timestamp),
+			}
+
+			err = conn.WriteJSON(logMessage)
+
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+		}
+	} else {
+		scanner := bufio.NewScanner(reader)
+
+		logLines := []*LogMessage{}
+
+		for scanner.Scan() {
+			textSplitted := strings.SplitN(scanner.Text(), " ", 2)
+			logLine := new(LogMessage)
+			logLine.Timestamp, logLine.Message = textSplitted[0], textSplitted[1]
+
+			logLines = append(logLines, logLine)
+		}
+
+		if err := scanner.Err(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		for _, log := range logLines {
+			err = conn.WriteJSON(log)
+
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+		}
+	}
+}
--- server/main.go
+++ server/main.go
@@ -1,221 +1,22 @@
 package main
 
 import (
-	"bufio"
-	"context"
-	"encoding/binary"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"strings"
+	"github.com/vinimdocarmo/dashker/controller"
 
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 )
 
-type ContainerListFilters struct {
-	ID string
-}
-
-type LogMessage struct {
-	Timestamp string `json:"timestamp"`
-	Message   string `json:"message"`
-}
-
-func getContainers() ([]types.Container, error) {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
-}
-
-func starContainer(id string) error {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-
-	if err != nil {
-		return err
-	}
-
-	err = cli.ContainerStart(context.Background(), id, types.ContainerStartOptions{})
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func stopContainer(id string) error {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-
-	if err != nil {
-		return err
-	}
-
-	err = cli.ContainerStop(context.Background(), id, nil)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func removeContainer(id string) error {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-
-	if err != nil {
-		return err
-	}
-
-	err = cli.ContainerRemove(context.Background(), id, types.ContainerRemoveOptions{Force: true})
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		for _, origin := range r.Header["Origin"] {
-			if origin == "http://localhost:3000" {
-				return true
-			}
-		}
-		return false
-	},
-}
-
-func socketHandler(id string, w http.ResponseWriter, r *http.Request) {
-	// Upgrade our raw HTTP connection to a websocket based one
-	conn, err := upgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		fmt.Print("Error during connection upgradation:", err)
-		return
-	}
-
-	defer conn.Close()
-
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-
-	if err != nil {
-		fmt.Println("Error creating new docker client: ", err)
-		return
-	}
-
-	reader, err := cli.ContainerLogs(context.Background(), id, types.ContainerLogsOptions{Tail: "100", Follow: true, ShowStdout: true, ShowStderr: true, Timestamps: true})
-
-	if err != nil {
-		fmt.Println("Error fetching container logs: ", err)
-		return
-	}
-
-	defer reader.Close()
-
-	container, err := cli.ContainerInspect(context.Background(), id)
-
-	if err != nil {
-		fmt.Println("Error inspecting container: ", err)
-		return
-	}
-
-	if !container.Config.Tty {
-		fmt.Println("container is NOT attached to TTY")
-
-		for {
-			header := make([]byte, 8)
-
-			n, err := reader.Read(header)
-
-			if err != nil || n != 8 {
-				if err == io.EOF {
-					fmt.Println("EOF reading header")
-					break
-				}
-				fmt.Println("Error during header reading:", err)
-				break
-			}
-
-			timestamp := make([]byte, 31) // 30 bytes for timestamp and 1 for the following space
-
-			n, err = reader.Read(timestamp)
-
-			if err != nil || n != 31 {
-				if err == io.EOF {
-					fmt.Println("EOF reading timestamp")
-					break
-				}
-				fmt.Println("Error during timestamp reading:", err)
-				break
-			}
-
-			outputSize := binary.BigEndian.Uint32(header[4:])
-			output := make([]byte, outputSize-uint32(n)) // makes sure to remove size of timestamp read
-			n, err = reader.Read(output)
-
-			if err != nil {
-				if err == io.EOF {
-					fmt.Println("EOF reading output")
-					break
-				}
-				fmt.Println("Error during output reading:", err)
-				break
-			}
-
-			logMessage := LogMessage{
-				Message:   string(output[:n]),
-				Timestamp: string(timestamp),
-			}
-
-			err = conn.WriteJSON(logMessage)
-
-			if err != nil {
-				fmt.Println("Error during message writing:", err)
-				break
-			}
-		}
-	} else {
-		fmt.Println("container is attached to TTY")
-
-		scanner := bufio.NewScanner(reader)
-
-		logLines := []*LogMessage{}
-
-		for scanner.Scan() {
-			textSplitted := strings.SplitN(scanner.Text(), " ", 2)
-			logLine := new(LogMessage)
-			logLine.Timestamp, logLine.Message = textSplitted[0], textSplitted[1]
-
-			logLines = append(logLines, logLine)
-		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "error reading raeder: ", err)
-			return
-		}
-
-		for _, log := range logLines {
-			err = conn.WriteJSON(log)
-
-			if err != nil {
-				fmt.Println("Error during message writing:", err)
-				break
-			}
-		}
-	}
-}
-
 func main() {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+
+	if err != nil {
+		panic(err)
+	}
+
+	dockerCtrl := controller.DockerController{DockerClient: cli}
+
 	r := gin.Default()
 
 	// - No origin allowed by default
@@ -227,63 +28,11 @@ func main() {
 
 	r.Use(cors.New(config))
 
-	r.GET("ws/container/:id/logs", func(c *gin.Context) {
-		id := c.Params.ByName("id")
-		socketHandler(id, c.Writer, c.Request)
-	})
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.GET("/container", func(c *gin.Context) {
-		containers, err := getContainers()
+	r.GET("ws/container/:id/logs", dockerCtrl.Logs)
+	r.GET("/container", dockerCtrl.List)
+	r.PUT("/container/:id/start", dockerCtrl.Start)
+	r.PUT("/container/:id/stop", dockerCtrl.Stop)
+	r.PUT("/container/:id/remove", dockerCtrl.Remove)
 
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(200, containers)
-	})
-	r.PUT("/container/:id/start", func(c *gin.Context) {
-		id := c.Params.ByName("id")
-
-		err := starContainer(id)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-	})
-	r.PUT("/container/:id/stop", func(c *gin.Context) {
-		id := c.Params.ByName("id")
-
-		err := stopContainer(id)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-	})
-	r.PUT("/container/:id/remove", func(c *gin.Context) {
-		id := c.Params.ByName("id")
-
-		err := removeContainer(id)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-	})
-
-	r.Run(":3001") // listen and serve on 0.0.0.0:8080
+	r.Run(":3001")
 }
