84c508700ee23703421848526fc5e2817b176ee8
Vinícius do Carmo <vinimdocarmo@gmail.com>
2021-08-09 01:50:27 +0200

    feat: add bottom action to container log component

--- client/src/components/ContainerLogs.tsx
+++ client/src/components/ContainerLogs.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 import cls from "classnames";
 import useWebSocket, { ReadyState } from "react-use-websocket";
+import { HiSearch, HiX } from "react-icons/hi";
 
-export const ContainerLogs: React.FC<{ containerId: string, onClose?: () => void }> = ({
-  containerId,
-  onClose = () => {},
-}) => {
+export const ContainerLogs: React.FC<{
+  containerId: string;
+  onClose?: () => void;
+}> = ({ containerId, onClose = () => {} }) => {
   const logBottomEl = useRef<HTMLDivElement>(null);
   const logTerminalEl = useRef<HTMLDivElement>(null);
   const [socketOpen, setSocketOpen] = useState(true);
+  const [stickToBottom, setStickToBottom] = useState(true);
   const [logsHistory, setLogsLineHisoty] = useState<
     { message: string; timestamp: string }[] | null
   >(null);
@@ -21,9 +23,10 @@ export const ContainerLogs: React.FC<{ containerId: string, onClose?: () => void
       onOpen: () => {
         console.info("connection opened for container ", containerId);
       },
+      onMessage: () => console.debug("new message"),
       // shouldReconnect: () => true // TODO: implement reconnect, but pass last timestamp as argument
     },
-    socketOpen
+    socketOpen // TODO: I don't think this is working properly
   );
 
   useEffect(() => {
@@ -40,63 +43,86 @@ export const ContainerLogs: React.FC<{ containerId: string, onClose?: () => void
 
   // Scroll terminal to bottom as soon as new logs arrive
   useEffect(() => {
-    if (logsHistory && logsHistory.length > 0) {
+    if (logsHistory && logsHistory.length > 0 && stickToBottom) {
       logTerminalEl.current?.scrollTo({ top: logBottomEl.current?.offsetTop });
     }
-  }, [logsHistory]);
+  }, [logsHistory, stickToBottom]);
   return (
-    <div className="resize-y bg-gray-900 border-2 border-gray-500 p-2 mt-3 font-mono text-xs relative">
-      <span
-        className="flex h-3 w-3 absolute -top-1.5 -left-1.5"
-        title="Streaming logs"
-        onClick={() => setSocketOpen(false)}
-      >
+    <div>
+      <div className="resize-y bg-gray-900 p-2 mt-3 font-mono text-xs relative">
         <span
-          className={cls(
-            "animate-ping absolute inline-flex h-full w-full rounded-full opacity-75",
-            {
-              "bg-green-400": readyState === ReadyState.OPEN,
-              "bg-red-400": readyState === ReadyState.CLOSED,
-              "bg-blue-400":
+          className="flex h-3 w-3 absolute -top-1.5 -left-1.5"
+          title="Streaming logs"
+          onClick={() => setSocketOpen(false)}
+        >
+          <span
+            className={cls(
+              "animate-ping absolute inline-flex h-full w-full rounded-full opacity-75",
+              {
+                "bg-green-400": readyState === ReadyState.OPEN,
+                "bg-red-400": readyState === ReadyState.CLOSED,
+                "bg-blue-400":
+                  readyState !== ReadyState.OPEN &&
+                  readyState !== ReadyState.CLOSED,
+              }
+            )}
+          ></span>
+          <span
+            className={cls("relative inline-flex rounded-full h-3 w-3", {
+              "bg-green-500": readyState === ReadyState.OPEN,
+              "bg-red-500": readyState === ReadyState.CLOSED,
+              "bg-blue-500":
                 readyState !== ReadyState.OPEN &&
                 readyState !== ReadyState.CLOSED,
-            }
-          )}
-        ></span>
-        <span
-          className={cls("relative inline-flex rounded-full h-3 w-3", {
-            "bg-green-500": readyState === ReadyState.OPEN,
-            "bg-red-500": readyState === ReadyState.CLOSED,
-            "bg-blue-500":
-              readyState !== ReadyState.OPEN &&
-              readyState !== ReadyState.CLOSED,
-          })}
-        ></span>
-      </span>
-      <span
-        className="text-white underline hover:cursor-pointer absolute top-1 right-2"
-        onClick={() => {
-          setSocketOpen(true);
-          setLogsLineHisoty([]);
-          onClose();
-        }}
-      >
-        close
-      </span>
-      <div
-        ref={logTerminalEl}
-        className="min-h-60 h-80 text-green-500 flex flex-col overflow-y-auto"
-      >
-        {logsHistory === null
-          ? "[Empty log]"
-          : logsHistory.map((log, i) => (
-              <div
-                key={log.timestamp}
-                ref={i === logsHistory.length - 1 ? logBottomEl : null} // set ref of the last message element
-              >
-                {log.message}
-              </div>
-            ))}
+            })}
+          ></span>
+        </span>
+        <button
+          className="absolute top-2 right-2"
+          onClick={() => {
+            setSocketOpen(true);
+            setLogsLineHisoty([]);
+            onClose();
+          }}
+        >
+          <HiX size="15px" color="#FFF" />
+        </button>
+        <div
+          ref={logTerminalEl}
+          className="min-h-60 h-80 text-green-500 flex flex-col overflow-y-auto"
+        >
+          {logsHistory === null
+            ? "[Empty log]"
+            : logsHistory.map((log, i) => (
+                <div
+                  key={log.timestamp}
+                  ref={i === logsHistory.length - 1 ? logBottomEl : null} // set ref of the last message element
+                >
+                  {log.message}
+                </div>
+              ))}
+        </div>
+      </div>
+      <div className="flex items-center justify-between bg-gray-800 text-gray-400 py-2 px-3">
+        <div className="flex items-center">
+          <HiSearch size="20" className="text-gray-400 mr-1" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-96 appearance-none bg-gray-800 border-none py-1 px-2 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex items-center space-x-1">
+          <input
+            type="checkbox"
+            id="sticky"
+            name="sticky"
+            checked={stickToBottom}
+            className="mr-1"
+            onChange={(ev) => setStickToBottom(ev.target.checked)}
+          />
+          <label htmlFor="sticky">Stick to bottom</label>
+        </div>
       </div>
     </div>
   );
