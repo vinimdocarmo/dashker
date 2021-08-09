a1a65c959411f608456a48d370b16d3ecb18ae30
Vinícius do Carmo <vinimdocarmo@gmail.com>
2021-08-09 12:05:42 +0200

    feat: improve main layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { HiCode } from "react-icons/hi";
 import { getContainers } from "./api";
 import { DockerContainer } from "./api/types/container.type";
 import { ContainerContainer } from "./containers/ContainerContainer";
@@ -13,26 +14,75 @@ function App() {
   }, []);
 
   return (
-    <div className=" bg-blue-200 min-h-screen">
-      <h1 className="text-xl">Docker dashboard</h1>
-      {containers.map(({ id, name, status, state, image }) => (
-        <ContainerContainer
-          key={id}
-          id={id}
-          name={name}
-          image={image}
-          status={status}
-          state={state}
-          onContainerRemove={(id) => {
-            setContainers((oldContainers) => {
-              return [
-                ...oldContainers.filter((container) => container.id !== id),
-              ];
-            });
-          }}
-        />
-      ))}
-    </div>
+    <main className="bg-blue-200 min-h-screen">
+      <nav className="flex items-center justify-between flex-wrap bg-gray-900 text-gray-200 p-4">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <span className="font-semibold text-xl tracking-tight">Dashker</span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Docs
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Examples
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Blog
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://github.com/vinimdocarmo/dashker"
+              className="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 inline-flex items-center"
+            >
+              <HiCode size="20px" className="inline-block mr-2" />
+              GitHub
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div className="container mx-auto mt-4">
+        {containers.map(({ id, name, status, state, image }) => (
+          <ContainerContainer
+            key={id}
+            id={id}
+            name={name}
+            image={image}
+            status={status}
+            state={state}
+            onContainerRemove={(id) => {
+              setContainers((oldContainers) => {
+                return [
+                  ...oldContainers.filter((container) => container.id !== id),
+                ];
+              });
+            }}
+          />
+        ))}
+      </div>
+    </main>
   );
 }
 
